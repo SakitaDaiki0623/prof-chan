eaa305e5e52c16a7ec7e82098155dd19f7c49898
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-04 22:21:04 +0900

    UPDATE: Tailwindでフラッシュメッセージのスタイルを指定

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,15 +8,19 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(new_profile_path, notice: 'Login successful')
+      if @user.profile.present?
+        redirect_to profiles_path, notice: 'ログインに成功しました'
+      else
+        redirect_to new_profile_path
+      end
     else
-      flash.now[:alert] = 'Login failed'
+      flash.now[:alert] = 'ログインに失敗しました'
       render action: 'new'
     end
   end
 
   def destroy
     logout
-    redirect_to(:root, notice: 'Logged out!')
+    redirect_to(:root, notice: 'ログアウトしました')
   end
 end
