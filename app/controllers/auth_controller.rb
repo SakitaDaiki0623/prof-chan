f31f54932e4e803a51e40f7c74066e17c5348ef7
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-21 09:42:00 +0900

    DELETE: gemなしのログインロジックを削除

--- app/controllers/auth_controller.rb
+++
@@ -1,8 +0,0 @@
-# app/controllers/auth_controller.rb
-class AuthController < ApplicationController
-  def callback
-    render plain: { access_token: request.env['omniauth.strategy'].access_token.to_hash,
-      auth_hash:  request.env['omniauth.auth']
-    }.to_yaml
-  end
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 # app/controllers/home_controller.rb
 class HomeController < ApplicationController
-  # skip_before_action :require_login
   def index; end
 end
