0d496eb032d443315219edc767b349e9f2786cff
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-22 02:59:09 +0900

    ADD: user認証時の項目を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable
   has_one :profile, dependent: :destroy
 
-  def self.from_omniauth(auth, access_token)
+  def self.from_omniauth(auth, user_token)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
-    user.name = auth.info.name
+    user_info = user_token.get("/api/users.identity").parsed.dig("user")
+    user.name = user_info["name"]
+    user.image = user_info["image_72"]
+    user.email = user_info["email"]
+    user.password = Devise.friendly_token[0, 20] # ランダムなパスワードを作成
     user.provider = auth.provider
     user.slack_credential_token = auth.credentials.token
     user.save
-    binding.pry
     user
   end
 end
