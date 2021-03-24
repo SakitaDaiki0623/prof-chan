09787d15482d74ab1f43f6bc94a9eb34d502ca95
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-24 10:07:12 +0900

    UPDATE: uidにunique制約を追加

--- db/migrate/20210322162845_add_team_id_to_users.rb
+++ db/migrate/20210322162845_add_team_id_to_users.rb
@@ -1,7 +1,5 @@
 class AddTeamIdToUsers < ActiveRecord::Migration[6.0]
   def change
-    change_column :users, :team_id, :string, null: false
-    change_column :users, :email, :string, null: false, unique: true
-    change_column :users, :uid, :string, null: false, unique: true
+    add_index     :users, :uid, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_03_22_162845) do
     t.string "team_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
   add_foreign_key "profiles", "users"
