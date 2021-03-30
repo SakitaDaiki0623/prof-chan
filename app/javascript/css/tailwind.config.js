004c3dde3f6bf5c8000d1c8b61c810d7b0432248
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-31 07:09:14 +0900

    ADD: Tailwindで背景画像の設定

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -1,21 +1,31 @@
 // app/javascript/css/tailwind.config.js
 module.exports = {
   purge: [
-    './app/**/*.html.erb',
-    './app/**/*.html.slim',
-    './app/**/*.js.erb',
-    './app/helpers/**/*.rb',
+    "./app/**/*.html.erb",
+    "./app/**/*.html.slim",
+    "./app/**/*.js.erb",
+    "./app/helpers/**/*.rb",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        "note": "url('/app/assets/images/note_bg.jpg')",
+      }),
       fontFamily: {
-        'prof-default': ['TsukuARdGothic-Regular','klee','Hannotate SC','Chalkboard SE','Comic Sans MS', 'sans-serif'],
-      }
+        "prof-default": [
+          "TsukuARdGothic-Regular",
+          "klee",
+          "Hannotate SC",
+          "Chalkboard SE",
+          "Comic Sans MS",
+          "sans-serif",
+        ],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
