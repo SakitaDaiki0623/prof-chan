a800b66f3be4c460bc1d2612a47e4a5374a12548
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-03-31 20:48:59 +0900

    ADD: TeamモデルのFactoryBotを作成

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -27,11 +27,11 @@ FactoryBot.define do
   factory :user do
     sequence(:name, 'user_1')
     sequence(:email)   { |n| "sample#{n}@example.com" }
-    team_id { rand(10 ** 19).to_s }
     uid     { rand(10 ** 19).to_s }
     slack_credential_token { rand(10 ** 19).to_s }
     provider { 'slack' }
     password { 'password' }
     image { 'https://avatars.slack-edge.com/2021-03-08/1825255374918_7955fb79fef81b734c2a_34.jpg' }
+    association :team
   end
 end
