4cb33e58a6703e27401c5ca93ad63847e8b22179
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-02 16:13:09 +0900

    ADD: ホームページのシステムスペックに追記

--- spec/system/home_pages_spec.rb
+++ spec/system/home_pages_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe "HomePages", type: :system do
 
-  context 'ホームページにアクセス' do
+  context 'ホームページにアクセス', focus: true do
     before { visit root_path }
     it 'slackログインボタンが表示されていること' do
       expect(page).to have_content('Slackログイン'), '「Slackログイン」ボタンが表示されていません'
+    end
+
+    it 'フッターが表示されていること' do
+      expect(page).to have_content('プロフちゃんとは'), 'プロフちゃんとはが表示されていません'
+      expect(page).to have_content('利用規約'), '「利用規約」が表示されていません'
+    end
+
+    it 'タイトルが「プロフちゃん」であること' do
+      expect(page).to have_title('プロフちゃん'), '意図したタイトルが表示されていません'
     end
   end
 
