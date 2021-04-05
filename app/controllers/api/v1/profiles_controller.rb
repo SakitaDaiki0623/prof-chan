74cbcd06241c0666511d5f8b3fcbb1762089bdbe
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-05 11:40:20 +0900

    [ADD] Api::V1::ProfilesControllerを作成

---
+++ app/controllers/api/v1/profiles_controller.rb
@@ -0,0 +1,47 @@
+class Api::V1::ProfilesController < ApplicationController
+  before_action :check_profile_presence, only: %i[new create]
+  skip_before_action :check_profile_nil, only: %i[new create]
+
+  def index
+    user = User.find(current_user.id)
+    @profiles = Profile.includes(user: :team).where(teams: { workspace_id: user.team.workspace_id })
+  end
+
+  def create
+    @profile = current_user.build_profile(profile_params)
+
+    if @profile.save
+      flash[:notice] = 'プロフィール作成が完了しました'
+      redirect_to profiles_path
+    else
+      flash.now[:alert] = 'プロフィール作成に失敗しました'
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update; end
+
+  def destroy; end
+
+  def new
+    user = User.select(:name, :image).find(current_user.id)
+    render json: user
+  end
+
+  private
+
+  def check_profile_presence
+    if current_user.profile.present?
+      flash[:notice] = '基本情報はもう作ったよ！'
+      redirect_to profiles_path
+    end
+  end
+
+  def profile_params
+    params.require(:profile).permit(:birthday, :day_of_joinning, :height, :gender, :blood_type, :prefecture_id)
+  end
+end
