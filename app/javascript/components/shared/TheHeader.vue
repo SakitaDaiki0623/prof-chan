3ba8a0c927b3bf7949b26638269fd92fbc97e7eb
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-06 19:39:44 +0900

    [UPDATE] ヘッダーコンポーネントの場所を変更

---
+++ app/javascript/components/shared/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex justify-between leading-10">
+    <img
+      width="6%"
+      height="6%"
+      class="ml-5"
+      src="../../tail_images/logo_transparent.png"
+    /><a
+      class="no-underline font-bold mr-7 mt-5"
+      rel="nofollow"
+      data-method="delete"
+      href="/logout"
+      >ログアウト</a
+    >
+  </div>
+</template>
+
+
