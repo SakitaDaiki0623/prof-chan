7119d8c3d76b2d9c941d838a553c6f0d3d3111df
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-07 06:05:14 +0900

    [UPDATE] ログイン済みのユーザーはプロフィール一覧にリダイレクトされるように実装

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,9 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
   skip_before_action :check_profile_nil, only: %i[index]
   layout 'top'
-  def index; end
+  def index
+    if user_signed_in?
+      redirect_to profiles_path
+    end
+  end
 end
