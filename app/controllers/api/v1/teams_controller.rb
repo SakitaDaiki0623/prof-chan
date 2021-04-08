5970b73eec3e04106c0a7e2056e697b63116d1be
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-08 18:49:32 +0900

    [ADD] moduleでコントローラを階層化

--- app/controllers/api/v1/teams_controller.rb
+++ app/controllers/api/v1/teams_controller.rb
@@ -1,12 +1,16 @@
-class Api::V1::TeamsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Api
+  module V1
+    class TeamsController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
-  def show
-    @user = User.find(current_user.id)
-    @team = Team.includes(:users).where( workspace_id: @user.team.workspace_id )
-    render json: ActiveModel::Serializer::CollectionSerializer.new(
-      @team,
-      serializer: TeamSerializer
-    ).to_json
+      def show
+        @user = User.find(current_user.id)
+        @team = Team.includes(:users).where(workspace_id: @user.team.workspace_id)
+        render json: ActiveModel::Serializer::CollectionSerializer.new(
+          @team,
+          serializer: TeamSerializer
+        ).to_json
+      end
+    end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,21 @@
-class Api::V1::UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+module Api
+  module V1
+    class UsersController < ApplicationController
+      skip_before_action :verify_authenticity_token
 
-  def index
-    @user = User.find(current_user.id)
-    @users = User.includes(:team).where(teams: { workspace_id: @user.team.workspace_id })
-    render json: ActiveModel::Serializer::CollectionSerializer.new(
-      @users,
-      serializer: UserSerializer
-    ).to_json
-  end
+      def index
+        @user = User.find(current_user.id)
+        @users = User.includes(:team).where(teams: { workspace_id: @user.team.workspace_id })
+        render json: ActiveModel::Serializer::CollectionSerializer.new(
+          @users,
+          serializer: UserSerializer
+        ).to_json
+      end
 
-  def show
-    @user = User.find(current_user.id)
-    render json: @user
+      def show
+        @user = User.find(current_user.id)
+        render json: @user
+      end
+    end
   end
 end
