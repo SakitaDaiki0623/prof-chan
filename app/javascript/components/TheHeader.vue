11775743b02a2ef52ab6dfa9b0bea70fc58a4fb2
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-12 00:15:22 +0900

    [ADD] 編集ページを作成

--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -1,3 +1,4 @@
+<!-- app/javascript/components/TheHeader.vue -->
 <template>
   <v-app-bar class="font-prof-default" color="gray accent-4" dark>
     <v-app-bar-nav-icon />
@@ -11,6 +12,10 @@
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
 
+    <v-btn icon @click="openProfileEditPage">
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
+
     <v-menu left bottom>
       <template #activator="{ on }">
         <v-btn icon v-on="on">
@@ -19,10 +24,34 @@
       </template>
 
       <v-list>
-        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-          <v-list-item-title>りんく {{ n }}</v-list-item-title>
+        <v-list-item v-for="n in 1" :key="n" @click="() => {}">
+          <v-list-item-title />
         </v-list-item>
       </v-list>
     </v-menu>
   </v-app-bar>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      user: {},
+    };
+  },
+  created() {
+    // [TODO: リファクタリング] axiosのモジュールに移すか考える
+    this.$axios
+      .get("/users/new")
+      .then((response) => (this.user = response.data))
+      .catch((err) => console.log(err.status));
+  },
+  methods: {
+    openProfileEditPage() {
+      this.$router.push(`/profiles/${this.user.profile.id}/edit`);
+    },
+  },
+};
+</script>
--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from "vue-router";
 import ProfilesPage from "../pages/profile/index";
 import ShowProfilesPage from "../pages/profile/show";
 import NewProfilesPage from "../pages/profile/new";
+import EditProfilesPage from "../pages/profile/edit";
 
 Vue.use(VueRouter);
 
@@ -19,9 +20,15 @@ const routes = [
     component: ProfilesPage,
   },
   {
-    path: "/profiles/:id",
+    path: "/profiles/:id(\\d+)",
     name: "ShowProfilesPage",
     component: ShowProfilesPage,
+    props: true
+  },
+  {
+    path: "/profiles/:id(\\d+)/edit",
+    name: "EditProfilesPage",
+    component: EditProfilesPage,
     props: true
   },
 ];
