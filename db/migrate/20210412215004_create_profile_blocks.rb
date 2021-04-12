28e489287a2a5b61ec62b96855d37d71105ff052
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-13 07:21:28 +0900

    [ADD] ProfileBlockモデルを作成

---
+++ db/migrate/20210412215004_create_profile_blocks.rb
@@ -0,0 +1,9 @@
+class CreateProfileBlocks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :profile_blocks do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
