4e0bb81f34343fb2b07aec38d9fb706f1ca1d54e
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-14 06:56:07 +0900

    [ADD] テキストブロック作成機能を追加

---
+++ app/javascript/components/TextProfCardList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="4"
+        v-for="textBlock in myTextBlocks"
+        :key="textBlock.id"
+      >
+        <v-card class="bg-prof-card bg-cover shadow rounded-2xl p-10" height="320" width="500">
+          <p class="text-4xl font-bold text-gray-600">
+            {{ textBlock.title }}
+          </p>
+          <v-card height="200" class="p-5 rounded-lg" outlined>
+          {{ textBlock.text }}
+          </v-card>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    myTextBlocks: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
--- app/javascript/pages/profile/edit.vue
+++ app/javascript/pages/profile/edit.vue
@@ -1,36 +1,51 @@
 <!-- app/javascript/pages/profile/show.vue -->
-<!-- TODO: 編集画面は後ほど削除予定 -->
 <template>
   <div
     class="bg-backimage-02 bg-cover bg-fixed text-gray-600 font-prof-default"
   >
     <v-container>
-      <p class="text-5xl font-bold note mb-10">
-        プロフィール編集
-      </p>
+      <p class="text-5xl font-bold note mb-10">プロフィール編集</p>
       <v-row class="mb-10">
-        <v-col
-          cols="12"
-          sm="6"
-        >
-          <EditBasicProfCard :profile="profile" />
+        <v-col cols="12" sm="6">
+          <BasicProfCard :currentUser="currentUser" :profile="profile" />
         </v-col>
       </v-row>
+      <v-row justify="center">
+        <v-btn tile color="success" @click="openProfileBlockSelectDialog">
+          <v-icon left> mdi-plus </v-icon>
+          ブロックを追加する
+        </v-btn>
+      </v-row>
     </v-container>
+    <TextProfCardList :myTextBlocks="myTextBlocks" />
+    <ProfileBlockSelectDialog
+      :is-shown-profile-block-select-dialog="isShownProfileBlockSelectDialog"
+      @open-text-format-dialog="openTextFormatDialog"
+    />
+    <TextFormatDialog
+      :is-shown-text-format-dialog="isShownTextFormatDialog"
+      @close-text-format-dialog="closeTextFormatDialog"
+    />
   </div>
 </template>
 
 <script>
 // plugins
 import axios from "axios";
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
-// Component ----------
-import EditBasicProfCard from "../../components/EditBasicProfCard";
+// components ----------
+import BasicProfCard from "../../components/BasicProfCard";
+import ProfileBlockSelectDialog from "../../components/ProfileBlockSelectDialog";
+import TextFormatDialog from "../../components/TextFormatDialog";
+import TextProfCardList from "../../components/TextProfCardList";
 
 export default {
   components: {
-    EditBasicProfCard,
+    BasicProfCard,
+    ProfileBlockSelectDialog,
+    TextFormatDialog,
+    TextProfCardList,
   },
   props: {
     id: {
@@ -40,23 +55,66 @@ export default {
     },
   },
   data() {
-    return {};
+    return {
+      isShownProfileBlockSelectDialog: false,
+      isShownTextFormatDialog: false,
+    };
   },
   computed: {
     ...mapState("profiles", ["profiles"]),
+    ...mapState("textBlocks", ["textBlocks"]),
+    ...mapState("users", ["currentUser"]),
+
     profile() {
       return this.profiles.find((profile) => profile.id == this.id) || {};
     },
+    myTextBlocks() {
+      return (
+        this.textBlocks.filter(
+          (textBlock) => textBlock.profile_block.id == this.currentUser.id
+        ) || {}
+      );
+    },
   },
   mounted() {
+    this.fetchProfiles();
+    this.fetchTextBlocks();
+    this.fetchCurrentUser();
+
     document.title = `プロフィール編集 - プロフちゃん`;
   },
   methods: {
-    moveToProfilesPage() {
-      this.$router.push("/profiles");
+    ...mapActions("profiles", ["fetchProfiles"]),
+    ...mapActions("textBlocks", ["fetchTextBlocks"]),
+    ...mapActions("users", ["fetchCurrentUser"]),
+
+    openProfileBlockSelectDialog() {
+      this.isShownProfileBlockSelectDialog = true;
+    },
+    closeProfileBlockSelectDialog() {
+      this.isShownProfileBlockSelectDialog = false;
+    },
+    openTextFormatDialog() {
+      this.closeProfileBlockSelectDialog();
+      this.isShownTextFormatDialog = true;
+    },
+    closeTextFormatDialog() {
+      this.isShownTextFormatDialog = false;
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.note {
+  width: 80%;
+  padding: 0 1em;
+  background: linear-gradient(#ccc 0.1px, transparent 0.6px) #fffbf2;
+  background-size: auto 2.5em;
+  line-height: 2.5em;
+  border-bottom: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  overflow: hidden;
+}
+</style>
