ed4b3ceb6f0ed0ab0a02bf71c2952bc9a4c4582a
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-16 21:22:55 +0900

    [ADD] vuex-persistedstateをインストール

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "../plugins/axios.js";
+// [参考]: https://qiita.com/_masa_u/items/b58b92c283f4e770e094
+import createPersistedState from "vuex-persistedstate";
 import { profiles } from "./modules/profiles";
 import { users } from "./modules/users";
 import { textBlocks } from "./modules/textBlocks";
@@ -16,4 +18,17 @@ export default new Vuex.Store({
     textBlocks,
     profileBlocks,
   },
+  plugins: [
+    createPersistedState({
+      // ストレージのキーを指定。デフォルトではvuex
+      key: "profChan",
+
+      // 管理対象のステートを指定。pathsを書かない時は`modules`に書いたモジュールに含まれるステート全て。`[]`の時はどれも保存されない
+      // paths: ["auth.isLoggedIn", "master.dataSelected"],
+      // paths: ["users.currentUser"],
+
+      // ストレージの種類を指定する。デフォルトではローカルストレージ
+      storage: window.sessionStorage,
+    }),
+  ],
 });
