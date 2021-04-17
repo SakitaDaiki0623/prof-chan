13d486ccac0815ee0fcd5ff5ff67290e41c54a11
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-17 15:06:00 +0900

    [ADD] flashメッセージのコンポーネントをインポート

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,17 +1,25 @@
 <template>
   <v-app id="app">
     <TheHeader />
+    <TheFlashMessage v-if="isFlash" />
     <router-view />
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import TheHeader from "./components/shared/TheHeader";
+import TheFlashMessage from "./components/shared/TheFlashMessage";
 
 export default {
-  components: { TheHeader },
-  data: function() {
+  components: { TheHeader, TheFlashMessage },
+  data() {
     return {};
+  },
+  computed: {
+    ...mapGetters({
+      isFlash: "flash/isFlash",
+    }),
   },
 };
 </script>
