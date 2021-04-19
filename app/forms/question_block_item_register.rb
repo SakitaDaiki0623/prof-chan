cf6cec85ee14d995c757363133acc2daf2a7df3e
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-19 19:40:51 +0900

    [FIX] Form Objectを修正

--- app/forms/question_block_item_register.rb
+++ app/forms/question_block_item_register.rb
@@ -25,11 +25,11 @@ class QuestionBlockItemRegister
   validates :profile_block_id,        presence: true
 
   def save
-    return if invalid?
+    return false if invalid?
     ActiveRecord::Base.transaction do
       question_block = QuestionBlock.create!(title: question_title, profile_block_id: profile_block_id)
 
-      question_block.question_items.create!(content: question_item_content1, answer: question_item_answer1) if question_item_content1.present?
+      question_block.question_items.create!(content: question_item_content1, answer: question_item_answer1)
       question_block.question_items.create!(content: question_item_content2, answer: question_item_answer2) if question_item_content2.present?
       question_block.question_items.create!(content: question_item_content3, answer: question_item_answer3) if question_item_content3.present?
     end
