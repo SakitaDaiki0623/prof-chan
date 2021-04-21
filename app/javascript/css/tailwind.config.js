aa89dd20f59db0878df6bb63b45d6fcfd7e0f473
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-22 06:59:04 +0900

    [ADD] TaiwindのPurgeファイルを追加

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -9,13 +9,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        backimage: "url('../images/backimage01.jpg')",
-        "backimage-02": "url('../images/backimage02.jpg')",
-        "prof-card": "url('../images/question_block_bg.jpg')",
-        "text-prof-block": "url('../images/text_block_bg.jpg')",
-        "question-prof-block": "url('../images/question_block_bg.jpg')",
-      }),
+      // backgroundImage: (theme) => ({
+      //   backimage: "url('../images/backimage01.jpg')",
+      //   "backimage-02": "url('../images/backimage02.jpg')",
+      //   "prof-card": "url('../images/question_block_bg.jpg')",
+      //   "text-prof-block": "url('../images/text_block_bg.jpg')",
+      //   "question-prof-block": "url('../images/question_block_bg.jpg')",
+      // }),
       fontFamily: {
         "prof-default": [
           "TsukuARdGothic-Regular",
