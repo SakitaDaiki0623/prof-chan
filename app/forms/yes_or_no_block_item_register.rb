0f6367d3788d9659391ae37605927f98bdff6693
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-24 13:53:46 +0900

    [ADD] フォームオブジェクト Yes or Noブロック

---
+++ app/forms/yes_or_no_block_item_register.rb
@@ -0,0 +1,38 @@
+# [参考] https://qiita.com/kumackey/items/b469143f1a0c4902cf4e
+# app/forms/yes_or_no_block_item_register.rb
+class YesOrNoBlockItemRegister
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActiveModel::Validations
+
+  attribute :yes_or_no_title,         :string
+  attribute :yes_or_no_item_content1, :string
+  attribute :yes_or_no_item_answer1,  :boolean
+  attribute :yes_or_no_item_content2, :string
+  attribute :yes_or_no_item_answer2,  :boolean
+  attribute :yes_or_no_item_content3, :string
+  attribute :yes_or_no_item_answer3,  :boolean
+  attribute :profile_block_id,       :integer
+
+  # validation =============
+  validates :yes_or_no_title,         presence: true,        length: { maximum: 50 }
+  validates :yes_or_no_item_content1, presence: true,        length: { maximum: 50 }
+  validates :yes_or_no_item_answer1
+  validates :yes_or_no_item_content2,                        length: { maximum: 50 }
+  validates :yes_or_no_item_answer2
+  validates :yes_or_no_item_content3,                        length: { maximum: 50 }
+  validates :yes_or_no_item_answer3
+  validates :profile_block_id,        presence: true
+
+  def save
+    return false if invalid?
+    ActiveRecord::Base.transaction do
+      yes_or_no_block = YesOrNoBlock.create!(title: yes_or_no_title, profile_block_id: profile_block_id)
+
+      yes_or_no_block.yes_or_no_items.create!(content: yes_or_no_item_content1, answer: yes_or_no_item_answer1)
+      yes_or_no_block.yes_or_no_items.create!(content: yes_or_no_item_content2, answer: yes_or_no_item_answer2) if yes_or_no_item_content2.present?
+      yes_or_no_block.yes_or_no_items.create!(content: yes_or_no_item_content3, answer: yes_or_no_item_answer3) if yes_or_no_item_content3.present?
+    end
+    true
+  end
+end
