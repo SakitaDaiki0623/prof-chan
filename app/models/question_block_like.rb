5a2832bda103e288a68b981a5c7ebf504d932795
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-25 13:56:08 +0900

    [ADD] 各いいね機能のコントローラ・モデル

--- app/models/question_block_like.rb
+++ app/models/question_block_like.rb
@@ -1,4 +1,6 @@
 class QuestionBlockLike < ApplicationRecord
   belongs_to :user
   belongs_to :question_block
+
+  scope :filter_by_question_block, ->(question_block_id) { where(question_block_id: question_block_id) if question_block_id }
 end
--- app/models/yes_or_no_block_like.rb
+++ app/models/yes_or_no_block_like.rb
@@ -1,4 +1,6 @@
 class YesOrNoBlockLike < ApplicationRecord
   belongs_to :user
   belongs_to :yes_or_no_block
+
+  scope :filter_by_yes_or_no_block, ->(yes_or_no_block_id) { where(yes_or_no_block_id: yes_or_no_block_id) if yes_or_no_block_id }
 end
