de8949b8917ef66aadf4e0d6fd1318374c3d3892
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-25 15:52:40 +0900

    [ADD:model] my_favorite_block

---
+++ app/models/my_favorite_block.rb
@@ -0,0 +1,25 @@
+# app/models/my_favorite_block.rb
+class MyFavoriteBlock < ApplicationRecord
+  # association ==========
+  belongs_to :profile_block
+
+  # validation =============
+  with_options length: { maximum: 15 } do
+    validates :manga_anime
+    validates :game_app
+    validates :character
+    validates :admired_person
+    validates :entertainer
+    validates :musician
+    validates :music
+    validates :food
+    validates :animal
+    validates :sports
+    validates :book
+    validates :place
+    validates :snack
+    validates :alcohol_drink
+    validates :restaurants
+    validates :characteristic
+  end
+end
---
+++ db/migrate/20210425064804_create_my_favorite_blocks.rb
@@ -0,0 +1,26 @@
+class CreateMyFavoriteBlocks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :my_favorite_blocks do |t|
+      t.string :manga_anime
+      t.string :game_app
+      t.string :character
+      t.string :admired_person
+      t.string :entertainer
+      t.string :musician
+      t.string :music
+      t.string :food
+      t.string :animal
+      t.string :sports
+      t.string :book
+      t.string :place
+      t.string :snack
+      t.string :alcohol_drink
+      t.string :restaurants
+      t.string :characteristic
+
+      t.references :profile_block, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
