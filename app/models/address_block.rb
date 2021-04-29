44797b623a70db154f028b2dc8b25885f5c01c7b
SakitaDaiki0623 <kurukurukurukuru1996@outlook.jp>
2021-04-29 13:28:28 +0900

    [UPDATE] 場所の変更

--- app/models/address_block.rb
+++ app/models/address_block.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: address_blocks
+#
+#  id                 :bigint           not null, primary key
+#  profile_block_id   :string(255)      not null
+#  full_address       :string(255)      not null
+#  postcode           :string(255)      not null
+#  twitter            :string(255)      not null
+#  instagram          :string(255)      not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+
+# app/models/address_block.rb
+
 class AddressBlock < ApplicationRecord
   # association ==========
   belongs_to :profile_block
