d180674a9325b54e4ca5b853c764c672c488a6e5
sakita daiki <kurukuruskt28@gmail.com>
2021-05-15 09:30:03 +0900

    [ADD] ブロック各種を人気順に並び替えるscopeを追加

--- app/models/concerns/blockable.rb
+++ app/models/concerns/blockable.rb
@@ -5,5 +5,6 @@ module Blockable
 
   included do
     scope :by_team, ->(user) { includes(profile_block: { user: :team }).where(teams: { workspace_id: User.find(user.id).team.workspace_id }) }
+    scope :popular_blocks, ->() { includes(:users).sort {|a,b| b.users.size <=> a.users.size} }
   end
 end
