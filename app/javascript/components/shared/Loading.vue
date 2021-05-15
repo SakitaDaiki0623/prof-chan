4c087618d797fb24f43fde8d9f949bbb1d844ab7
sakita daiki <kurukuruskt28@gmail.com>
2021-05-15 19:40:54 +0900

    [ADD] loading画面を仮実装

---
+++ app/javascript/components/shared/Loading.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="fullview">
+      <div class="loading-spacer"></div>
+      <vue-loading
+        type="spiningDubbles"
+        color="#aaa"
+        :size="{ width: '100px', height: '100px' }"
+      >
+      </vue-loading>
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueLoading } from "vue-loading-template";
+
+export default {
+  name: "loading",
+  components: {
+    VueLoading,
+  },
+};
+</script>
+
+<style>
+.fullview {
+  width: 100%;
+  height: 100%;
+  background: #fefefe;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.loading-spacer {
+  height: 30%;
+}
+</style>
