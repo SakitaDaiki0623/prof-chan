6c93f1743b4790ec3f6fd5c40e944c052a30135d
sakita daiki <kurukuruskt28@gmail.com>
2021-05-20 11:38:04 +0900

    [UPDATE] アプリ名変更

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ShowProfile
+module ProfChan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
