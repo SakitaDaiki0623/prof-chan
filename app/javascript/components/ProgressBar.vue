f3b33a3e14848575755a31407196b6853a796da6
sakita daiki <kurukuruskt28@gmail.com>
2021-05-21 19:40:52 +0900

    [ADD] progressbarコンポーネントを作成

---
+++ app/javascript/components/ProgressBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <v-progress-linear
+      :value="percentageForBlocks"
+      height="10"
+      striped
+      :color="blockColor"
+    >
+    </v-progress-linear>
+    <div v-if="percentageForBlocks <= 40">
+      <span class="text-lg text-blue-400 text-center p-5 bg-white rounded-full"
+        >充実度{{ percentageForBlocks }}%</span
+      >
+      <img src="../images/prof_sad.png" class="image" />
+    </div>
+    <div v-else-if="41 <= percentageForBlocks && percentageForBlocks <= 80">
+      <span class="text-lg text-center p-5 bg-white rounded-full"
+        >充実度{{ percentageForBlocks }}%</span
+      >
+      <img src="../images/prof_normal.png" class="image" />
+    </div>
+    <div v-else>
+      <span class="text-lg text-red-400 text-center p-5 bg-white rounded-full"
+        >充実度{{ percentageForBlocks }}%</span
+      >
+      <img src="../images/prof_happy.png" class="image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    percentageForBlocks: {
+      type: Number,
+      required: true,
+    },
+    blockColor: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  max-width: 5rem;
+  display: inline;
+}
+</style>
