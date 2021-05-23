ed50991c267cf6115c283ecf4ee301664dba9ba8
sakita daiki <kurukuruskt28@gmail.com>
2021-05-24 03:21:24 +0900

    [UPDATE] text blockコーナーを修正

--- app/javascript/components/text_block/TextBlockList.vue
+++ app/javascript/components/text_block/TextBlockList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container
-    class="rounded-2xl bg-brown-200"
+    class="mb-10 bg-brown-200"
     v-show="isMyTextBlocksLengthNotZero || isThisEditPage"
   >
     <div class="text-center text-5xl text-white py-5 my-5 top-sub-title">
@@ -47,13 +47,7 @@
           />
         </v-col>
       </transition-group>
-      <v-container v-else class="no-block-display-container">
-        <v-row justify="center">
-          <div class="font-bold text-2xl opacity-50">
-            社員のテキストブロックがありません
-          </div>
-        </v-row>
-      </v-container>
+      <NoBlockContainer block-name="テキスト" v-else />
     </div>
 
     <TextFormatDialog
@@ -72,12 +66,14 @@ import { mapState, mapActions, Store } from "vuex";
 import TextFormatDialog from "./TextFormatDialog";
 import TextBlockCard from "./TextBlockCard";
 import ProgressBar from "../ProgressBar";
+import NoBlockContainer from "../static/NoBlockContainer";
 
 export default {
   components: {
     TextFormatDialog,
     TextBlockCard,
     ProgressBar,
+    NoBlockContainer,
   },
   props: {
     isThisEditPage: {
