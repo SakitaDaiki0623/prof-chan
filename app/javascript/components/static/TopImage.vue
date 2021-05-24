d6b97366bda1ea6f90cad02d3f44d70685802823
sakita daiki <kurukuruskt28@gmail.com>
2021-05-25 01:27:12 +0900

    [ADD] 最近入社した社員を表示

---
+++ app/javascript/components/static/TopImage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="top-bg">
+    <div class="border-t-2 border-pink-500 border-dashed">
+      <v-row>
+        <v-col cols="12" sm="12">
+          <div class="text-center text-3xl">
+            - - - - REMOTELY PROMOTE YOURSELF.- - - -
+          </div>
+        </v-col>
+        <v-col cols="12" sm="8">
+          <div class="text-center py-20">
+            <div class="text-lg">
+              コロナ時代のフルリモート社員に贈る社員プロフィール公開サービス(slack)
+            </div>
+            <div class="text-8xl p-10 font-body">
+              プロフちゃん
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <img src="../../images/prof_happy.png" class="h-80" />
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
