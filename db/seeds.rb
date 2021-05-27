48c03d56c71f3d259714f29c6398034471a0bf00
sakita daiki <kurukuruskt28@gmail.com>
2021-05-27 17:20:32 +0900

    [UPDATE] 本番環境に投入用のseedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,33 +2,43 @@ require 'factory_bot'
 require 'faker'
 
 # create another team workspace and users belonging to it
-FactoryBot.create(:team) do |team|
-  FactoryBot.create_list(:user, 30, team: team)
-end
+if Rails.env.development?
 
-# create real team workspace and users
-FactoryBot.create(:team, :real_team) do |team|
-  FactoryBot.create_list(:user, 30, team: team)
-  random_five_users = User.all.shuffle.slice(0, 5)
-  random_ten_question_blocks = QuestionBlock.all.shuffle.slice(0, 10)
-  random_ten_ranking_blocks = RankingBlock.all.shuffle.slice(0, 10)
-  random_ten_text_blocks = TextBlock.all.shuffle.slice(0, 10)
-  random_ten_yes_or_no_blocks = YesOrNoBlock.all.shuffle.slice(0, 10)
-  random_five_users.each do |user|
-    random_ten_question_blocks.each do |block|
-      FactoryBot.create(:question_block_like, user: user, question_block: block)
-    end
-    random_ten_ranking_blocks.each do |block|
-      FactoryBot.create(:ranking_block_like, user: user, ranking_block: block)
-    end
-    random_ten_text_blocks.each do |block|
-      FactoryBot.create(:text_block_like, user: user, text_block: block)
-    end
-    random_ten_yes_or_no_blocks.each do |block|
-      FactoryBot.create(:yes_or_no_block_like, user: user, yes_or_no_block: block)
+  FactoryBot.create(:team) do |team|
+    FactoryBot.create_list(:user, 30, team: team)
+  end
+
+  # create real team workspace and users
+  FactoryBot.create(:team, :real_team) do |team|
+    FactoryBot.create_list(:user, 30, team: team)
+    random_five_users = User.all.shuffle.slice(0, 5)
+    random_ten_question_blocks = QuestionBlock.all.shuffle.slice(0, 10)
+    random_ten_ranking_blocks = RankingBlock.all.shuffle.slice(0, 10)
+    random_ten_text_blocks = TextBlock.all.shuffle.slice(0, 10)
+    random_ten_yes_or_no_blocks = YesOrNoBlock.all.shuffle.slice(0, 10)
+    random_five_users.each do |user|
+      random_ten_question_blocks.each do |block|
+        FactoryBot.create(:question_block_like, user: user, question_block: block)
+      end
+      random_ten_ranking_blocks.each do |block|
+        FactoryBot.create(:ranking_block_like, user: user, ranking_block: block)
+      end
+      random_ten_text_blocks.each do |block|
+        FactoryBot.create(:text_block_like, user: user, text_block: block)
+      end
+      random_ten_yes_or_no_blocks.each do |block|
+        FactoryBot.create(:yes_or_no_block_like, user: user, yes_or_no_block: block)
+      end
     end
   end
+  # 管理者の作成
+  FactoryBot.create(:user, :admin)
 end
 
-# 管理者の作成
-FactoryBot.create(:user, :admin)
+if Rails.env.production?
+  Team.create!(
+    name: '通常ログイン用チーム',
+    workspace_id: (0...8).map{ ('A'..'Z').to_a[rand(26)] }.join
+    image: "https://i.gyazo.com/f0c0826c1358634f1821320e5530f8ec.png"
+  )
+end
