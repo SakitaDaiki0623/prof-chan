f0130cebd60b204f64b174941498402f234e395f
sakita daiki <kurukuruskt28@gmail.com>
2021-05-27 11:40:50 +0900

    [UPDATE] tailwindのブレークポイントをvuetifyに調整

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: "600px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1264px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1904px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: (theme) => ({
         "prof-normal": "url('../images/prof_normal.png')",
