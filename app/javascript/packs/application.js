1e42b7c7d17e110303ab3ff82ec717fdb3a8c130
sakita daiki <kurukuruskt28@gmail.com>
2021-06-02 23:56:06 +0900

    [UPDATE]vue画面でブラウザバックで白画面になるのを防ぐ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require("@rails/ujs").start();
+require("@rails/ujs").start();
+// vue画面でブラウザバックで白画面になるのを防ぐ
 // require("turbolinks").start();
-// require("@rails/activestorage").start();
-// require("channels");
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
