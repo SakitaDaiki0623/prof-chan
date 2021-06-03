e4f067b49154d43ba722a01d5f540b7225cc40c6
sakita daiki <kurukuruskt28@gmail.com>
2021-06-03 13:25:01 +0900

    [UPDATE] 誕生日がいない時は誕生日欄を非表示

--- app/javascript/components/Birthday.vue
+++ app/javascript/components/Birthday.vue
@@ -1,52 +1,64 @@
 <template>
-  <v-row class="bg-brown-400 pA-3" justify="center" align-content="center">
-    <v-col cols="12" sm="12">
-      <div class="text-xl top-sub-title mb-5 lg:text-4xl lg:mb-10">
-        <v-icon large color="white">mdi-cake-variant</v-icon>
-        今月誕生日の社員さん
-        <v-icon large color="white">mdi-cake-variant</v-icon>
-      </div>
-    </v-col>
-    <v-col cols="12" sm="10">
-      <div
-        class="text-2xl lg:text-4xl text-white border-dashed border-t-8 border-b-8 border-white pb-2 inline-block"
-      >
-        HAPPY BIRTHDAY!!
-      </div>
-    </v-col>
-    <v-col
-      v-for="birthdayUserProfile in birthdayUserProfiles"
-      :key="birthdayUserProfile.public_uid"
-      cols="12"
-      sm="12"
-      md="3"
-      class="border-dotted border-white border-2 ma-5"
+  <div>
+    <v-row
+      class="bg-brown-400 pA-3"
+      justify="center"
+      align-content="center"
+      v-if="length !== 0"
     >
-      <v-card class="mx-auto" max-width="344" outlined color="brown lighten-5">
-        <v-card color="brown lighten-2" class="white--text">
-          誕生日 {{ birthdayUserProfile.birthday | moment }}
-        </v-card>
-        <v-img :src="birthdayUserProfile.user.image.url" height="180px" />
-
-        <div class="text-center font-bold text-4xl pt-5 text-brown-600">
-          {{ birthdayUserProfile.user.name }} さん
+      <v-col cols="12" sm="12">
+        <div class="text-xl top-sub-title mb-5 lg:text-4xl lg:mb-10">
+          <v-icon large color="white">mdi-cake-variant</v-icon>
+          今月誕生日の社員さん
+          <v-icon large color="white">mdi-cake-variant</v-icon>
         </div>
+      </v-col>
+      <v-col cols="12" sm="10">
+        <div
+          class="text-2xl lg:text-4xl text-white border-dashed border-t-8 border-b-8 border-white pb-2 inline-block"
+        >
+          HAPPY BIRTHDAY!!
+        </div>
+      </v-col>
+      <v-col
+        v-for="birthdayUserProfile in birthdayUserProfiles"
+        :key="birthdayUserProfile.public_uid"
+        cols="12"
+        sm="12"
+        md="3"
+        class="border-dotted border-white border-2 ma-5"
+      >
+        <v-card
+          class="mx-auto"
+          max-width="344"
+          outlined
+          color="brown lighten-5"
+        >
+          <v-card color="brown lighten-2" class="white--text">
+            誕生日 {{ birthdayUserProfile.birthday | moment }}
+          </v-card>
+          <v-img :src="birthdayUserProfile.user.image.url" height="180px" />
 
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="brown lighten-1"
-            class="white--text"
-            small
-            tile
-            @click="moveToUserProfilePage(birthdayUserProfile)"
-          >
-            社員プロフィールを見る
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+          <div class="text-center font-bold text-4xl pt-5 text-brown-600">
+            {{ birthdayUserProfile.user.name }} さん
+          </div>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn
+              color="brown lighten-1"
+              class="white--text"
+              small
+              tile
+              @click="moveToUserProfilePage(birthdayUserProfile)"
+            >
+              社員プロフィールを見る
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -63,6 +75,11 @@ export default {
     return {
       birthdayUserProfiles: [],
     };
+  },
+  computed: {
+    length() {
+      return this.birthdayUserProfiles.length;
+    },
   },
   mounted() {
     this.firstRead();
