ff3ad765c734c1c5620bd5d02018605d3a2eba20
sakita daiki <kurukuruskt28@gmail.com>
2021-06-07 16:36:23 +0900

    [ADD] ranking blockの投稿機能を実装

--- app/controllers/api/v1/ranking_blocks_controller.rb
+++ app/controllers/api/v1/ranking_blocks_controller.rb
@@ -3,6 +3,7 @@ module Api
   module V1
     class RankingBlocksController < ApiController
       before_action :set_ranking_block, only: %i[update destroy]
+      include SlackBlockKit
 
       def index
         @ranking_blocks = RankingBlock.by_team(current_user)
@@ -59,43 +60,11 @@ module Api
       def post_to_slack_after_create
         @ranking_block = current_user.profile_block.ranking_blocks.build(ranking_block_params)
         if @ranking_block.valid?
-          chat_post_message(@ranking_block)
+          post_ranking_block(@ranking_block)
           render json: @ranking_block, status: :no_content
-
         else
           render json: @ranking_block.errors, status: :bad_request
         end
-      end
-
-      def chat_post_message(block)
-        client = Slack::Web::Client.new
-        text = "*#{current_user.name}さんがランキングブロックを作成したよ:bangbang:*\n タイトル: :star2:*#{block.title}* :star2:"
-        client.chat_postMessage(
-          channel: '#プロフちゃん実験',
-          text: text,
-          blocks: [
-            {
-              "type": 'section',
-              "text": {
-                "type": 'mrkdwn',
-                "text": text
-              }
-            },
-            {
-              "type": 'divider'
-            },
-            {
-              "type": 'section',
-              "text": {
-                "type": 'mrkdwn',
-                "text": ":first_place_medal: *#{block.first_place}*\n- - - - - - - - - - - - - - - - - - - - - -\n:second_place_medal: *#{block.second_place}*\n- - - - - - - - - - - - - - - - - - - - - -\n:third_place_medal: *#{block.third_place}*"
-              }
-            },
-            {
-              "type": 'divider'
-            }
-          ]
-        )
       end
 
       private
