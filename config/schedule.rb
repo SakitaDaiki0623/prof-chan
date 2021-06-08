bc100c754d261d518bca7fc5364596451f714748
sakita daiki <kurukuruskt28@gmail.com>
2021-06-08 15:42:21 +0900

    [add: whereverの設定ファイルを追加]

---
+++ config/schedule.rb
@@ -0,0 +1,21 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+
+require File.expand_path(File.dirname(__FILE__) + '/environment')
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
+every :day, at: '9:00 pm' do
+  rake "blocks:post_blocks"
+end
+
+# Learn more: http://github.com/javan/whenever
