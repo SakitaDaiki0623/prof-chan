5a501a59d0953de157ada4f41192d243e0b88221
sakita daiki <kurukuruskt28@gmail.com>
2021-06-14 11:22:06 +0900

    add: チャンネルに初回ログイン時にメッセージが送られるように実装

--- app/controllers/concerns/slack_api_activatable.rb
+++ app/controllers/concerns/slack_api_activatable.rb
@@ -10,8 +10,8 @@ module SlackApiActivatable
       created_channel = create_channel_in_team(channel_name, access_token).dig('channel')
       invite_result = try_invite_user(info, created_channel, access_token)
       if invite_result.dig('ok')
-        post_welcome_message_to_channel(info, channel, access_token)
-        post_direct_message_to_user(info, channel, access_token)
+        post_welcome_message_to_channel(info, created_channel, access_token)
+        post_direct_message_to_user(info, created_channel, access_token)
       end
       created_channel
     else
@@ -47,7 +47,7 @@ module SlackApiActivatable
   def post_welcome_message_to_channel(info, channel, access_token)
     channel_id = channel.dig('id')
     user_name = info.dig('user', 'name')
-    encoded_mgs = get_block_kit_direct_msg(info, channel)
+    encoded_mgs = get_block_kit_welcome_msg(info)
     text = "@#{user_name}さんがプロフちゃんを始めました！"
     encoded_text = URI.encode_www_form_component(text)
     access_token.post("api/chat.postMessage?channel=#{channel_id}&blocks=#{encoded_mgs}&text=#{encoded_text}&pretty=1").parsed
@@ -64,14 +64,14 @@ module SlackApiActivatable
   def get_block_kit_welcome_msg(info)
     user_name = info.dig('user', 'name')
     user_image = info.dig('user', 'image_192')
-    message = "[ { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '@#{user_name}さんがプロフちゃんを始めました！:hamster:\n プロフを確認して気になる話題を探してみよう！:star: \n 共通の話題があると嬉しいな！' }, 'accessory': { 'type': 'image', 'image_url': '#{user_image}', 'alt_text': 'alt text for image' } }, { 'type': 'divider' } ]"
+    message = "[ { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '@here \n @#{user_name}さんがプロフちゃんを始めました！:hamster:\n プロフを確認して気になる話題を探してみよう！:star: \n 共通の話題があると嬉しいな！' }, 'accessory': { 'type': 'image', 'image_url': '#{user_image}', 'alt_text': 'alt text for image' } }, { 'type': 'divider' } ]"
     ERB::Util.url_encode(message)
   end
 
   def get_block_kit_direct_msg(info, channel)
     team_domain = info.dig('team', 'domain')
     channel_id = channel.dig('id')
-    message = "[ { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': 'プロフちゃんの世界へようこそ:musical_note:\nこれからよろしくお願いします！\n*<https://prof-chan.herokuapp.com/|プロフちゃんのサイト>*' } }, { 'type': 'divider' }, { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '基本的にプロフィールは<https://#{team_domain}.slack.com/archives/#{channel_id}/|#プロフ共有スペース>で共有するよ！あなたが入力した内容を共有チャンネルでシェアしよう！' } } ]"
+    message = "[ { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': 'プロフちゃんの世界へようこそ:musical_note:\nこれからよろしくお願いします:hamster:\n*<https://prof-chan.herokuapp.com/|プロフちゃんのサイト>*' } }, { 'type': 'divider' }, { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '基本的にプロフィールは<https://#{team_domain}.slack.com/archives/#{channel_id}/|#プロフ共有スペース>で共有するよ！あなたが入力した内容を共有チャンネルでシェアしよう！' } } ]"
     ERB::Util.url_encode(message)
   end
 end
