e3c63ca348cbb4a27dc2f0f7e99046aec31e572c
sakita daiki <kurukuruskt28@gmail.com>
2021-06-14 10:32:27 +0900

    add: プロフ共有スペースのリンクをDMに追加

--- app/controllers/concerns/slack_api_activatable.rb
+++ app/controllers/concerns/slack_api_activatable.rb
@@ -10,11 +10,11 @@ module SlackApiActivatable
     if channel.nil?
       created_channel = create_channel_in_team(channel_name, access_token).dig("channel")
       invite_result = try_invite_user(info, created_channel, access_token)
-      post_direct_message_to_user(info, access_token)
+      post_direct_message_to_user(info, created_channel, access_token)
       return created_channel
     else
       invite_result = try_invite_user(info, channel, access_token)
-      post_direct_message_to_user(info, access_token)
+      post_direct_message_to_user(info, channel, access_token)
       return channel
     end
   end
@@ -39,16 +39,18 @@ module SlackApiActivatable
     access_token.post("api/conversations.invite?channel=#{channel_id}&users=#{user_id}&pretty=1").parsed
   end
 
-  def post_direct_message_to_user(info, access_token)
+  def post_direct_message_to_user(info, channel, access_token)
     user_id = info.dig("user", "id")
-    encoded_mgs = get_block_kit_msg
+    encoded_mgs = get_block_kit_msg(info, channel)
     text = "プロフちゃんの世界へようこそ"
     encoded_text = URI.encode_www_form_component(text)
     access_token.post("api/chat.postMessage?channel=#{user_id}&blocks=#{encoded_mgs}&text=#{encoded_text}&pretty=1").parsed
   end
 
-  def get_block_kit_msg
-    message = '[ { "type": "section", "text": { "type": "mrkdwn", "text": "プロフちゃんの世界へようこそ:musical_note:\nこれからよろしくお願いします！\n*<https://prof-chan.herokuapp.com/|プロフちゃんのサイト>*" } }, { "type": "divider" }, { "type": "divider" }, { "type": "section", "text": { "type": "mrkdwn", "text": "基本的にプロフィールは#プロフ共有スペースで共有するよ！あなたが入力した内容を共有チャンネルでシェアしよう！" } } ]'
+  def get_block_kit_msg(info, channel)
+    team_domain = info.dig("team", "domain")
+    channel_id = channel.dig("id")
+    message = "[ { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': 'プロフちゃんの世界へようこそ:musical_note:\nこれからよろしくお願いします！\n*<https://prof-chan.herokuapp.com/|プロフちゃんのサイト>*' } }, { 'type': 'divider' }, { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '基本的にプロフィールは<https://#{team_domain}.slack.com/archives/#{channel_id}/|#プロフ共有スペース>で共有するよ！あなたが入力した内容を共有チャンネルでシェアしよう！' } } ]"
     return ERB::Util.url_encode(message)
   end
 
