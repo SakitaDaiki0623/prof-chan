a19463b9fa72e26602844c135b5596205d175a29
sakita daiki <kurukuruskt28@gmail.com>
2021-06-15 11:54:30 +0900

    update: 環境変数に移動

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 # config/initializers/sentry.rb
 Sentry.init do |config|
-  config.dsn = 'https://a50238bb57f840e2a4683cd6867c7740:3a79472eeb8647e09890e54d930d1c23@o849648.ingest.sentry.io/5816581'
+  config.dsn = ENV['SENTRY_DSN']
   config.breadcrumbs_logger = [:active_support_logger]
 
   # To activate performance monitoring, set one of these options.
