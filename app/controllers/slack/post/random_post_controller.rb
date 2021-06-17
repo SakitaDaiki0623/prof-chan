8aea18179fe0a12f592483e42e8815d97749396c
sakita daiki <kurukuruskt28@gmail.com>
2021-06-18 08:43:32 +0900

    update:ユーザ以外のブロックが投稿されるようにアップデート

--- app/controllers/slack/post/random_post_controller.rb
+++ app/controllers/slack/post/random_post_controller.rb
@@ -6,17 +6,24 @@ module Slack
       def random_block
         return if @user.nil?
         block = pick_up_block(@user)
-        return if block.nil?
-        encoded_msg = convert_block_msg(block)
-        post_block(@encoded_text, encoded_msg, @uid, @access_token)
+        if block.present?
+          text = "ランダムにブロックを送信:hamster:"
+          encoded_text = ERB::Util.url_encode(text)
+          encoded_msg = convert_block_msg(block)
+          post_block(encoded_text, encoded_msg, @uid, @access_token)
+        else
+          text = "まだあなた以外のブロックが作成されていないよ、、、:cry:"
+          encoded_text = ERB::Util.url_encode(text)
+          post_no_block(encoded_text, @uid, @access_token)
+        end
       end
 
       def pick_up_block(user)
-        favorite_block = FavoriteBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).sample
-        question_block = QuestionBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).sample
-        ranking_block = RankingBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).sample
-        yes_or_no_block = YesOrNoBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).sample
-        text_block = TextBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).sample
+        favorite_block = FavoriteBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).where.not(users: { id: user.id }).sample
+        question_block = QuestionBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).where.not(users: { id: user.id }).sample
+        ranking_block = RankingBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).where.not(users: { id: user.id }).sample
+        yes_or_no_block = YesOrNoBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).where.not(users: { id: user.id }).sample
+        text_block = TextBlock.all.includes(profile_block: { user: :team }).where(teams: { workspace_id: user.team.workspace_id }).where.not(users: { id: user.id }).sample
         block = [favorite_block, question_block, ranking_block, yes_or_no_block, text_block].sample
         return block
       end
@@ -34,12 +41,14 @@ module Slack
         access_token.post("api/chat.postMessage?channel=#{channel_id}&blocks=#{encoded_msg}&text=#{encoded_text}&pretty=1").parsed
       end
 
+      def post_no_block(encoded_text, channel_id, access_token)
+        access_token.post("api/chat.postMessage?channel=#{channel_id}&text=#{encoded_text}&pretty=1").parsed
+      end
+
       def set_user_team_token
         @user = User.find_by(uid: params[:user_id])
         return if @user.nil?
         @uid = @user.uid
-        @text = "ランダムにブロックを送信:hamster:"
-        @encoded_text = ERB::Util.url_encode(@text)
         @access_token = set_access_token(@user.authentication.access_token)
       end
     end
