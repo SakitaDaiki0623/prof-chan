c1baa5e6ff10a08e6741e60cce1e9d8d13fc7778
sakita daiki <kurukuruskt28@gmail.com>
2021-06-18 17:37:17 +0900

    update:ヘルプメッセージ送信を仮実装

--- app/controllers/slack/app_home_opened_controller.rb
+++ app/controllers/slack/app_home_opened_controller.rb
@@ -1,17 +1,33 @@
 module Slack
   class AppHomeOpenedController < Slack::ApplicationController
-    before_action :set_user_team_token, only: %i[actions respond]
+    before_action :set_user_team_token, only: %i[respond]
     def respond
-      if @team.nil? || @user.nil? || @team.workspace_id != @user.team.workspace_id
-        return
-      else
-        encoded_msg = encoded_block_msg(@team)
-        @access_token.post("api/views.publish?user_id=#{@user.uid}&view=#{encoded_msg}&pretty=1").parsed
+      return @team.nil? || @user.nil? || @team.workspace_id != @user.team.workspace_id
+      if params[:event][:type] == "app_home_opened"
+        publish_to_home_tab(@team, @user, @access_token)
+      elsif params[:event][:type] == "message"
+        send_help_msg(@team, @user, @access_token)
       end
     end
 
-    def encoded_block_msg(team)
+    def publish_to_home_tab(team, user, access_token)
+      encoded_msg = encoded_home_tab_block_msg(team)
+      @access_token.post("api/views.publish?user_id=#{user.uid}&view=#{encoded_msg}&pretty=1").parsed
+    end
+
+    def encoded_home_tab_block_msg(team)
       msg = "{ 'type': 'home', 'blocks': [ { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '*アプリ専用チャンネルについて* \n チームのプロフは<https://#{team.domain}.slack.com/archives/#{team.share_channel_id}/|#プロフ共有スペース>で共有するよ！\n あなたが入力した内容を共有チャンネルでシェアしよう！ \n :warning:<https://#{team.domain}.slack.com/archives/#{team.share_channel_id}/|#プロフ共有スペース>から退出するとあなたからの投稿ができなくなります' } }, { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '*投稿機能について*\n --------------------------------------------- \n :eight_spoked_asterisk:  各ブロックの投稿 \n 各ブロックを1日に一度投稿することができるよ:star:\nみんなに共有したいことを投稿してみよう！\n --------------------------------------------- \n:sparkle:  毎日18時の自動投稿 \n毎日18時にチーム内の各ブロックを投稿するよ:star: \n あなたのブロックが選ばれたら嬉しいな！' } }, { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '*Slashコマンドについて*\n本アプリでは以下のコマンドがSlack内で利用できるよ:hamster:' } }, { 'type': 'section', 'fields': [ { 'type': 'mrkdwn', 'text': ':information_source: `/prof_help` \nDMでヘルプメッセージを送るよ' }, { 'type': 'mrkdwn', 'text': ':postbox: `/prof_random_block` \n DMでランダムにブロックを1つ送るよ' } ] }, { 'type': 'section', 'fields': [ { 'type': 'mrkdwn', 'text': ':ok_hand: `/prof_activate_share` \n毎日18時の投稿を有効するよ' }, { 'type': 'mrkdwn', 'text': ':raised_back_of_hand: `/prof_inactivate_share` \n 毎日18時の投稿を止めるよ' } ] }, { 'type': 'divider' } ] }"
+      encoded_msg = ERB::Util.url_encode(msg)
+      return encoded_msg
+    end
+
+    def send_help_msg(team, user, access_token)
+      encoded_msg = encoded_help_block_msg(team)
+      @access_token.post("api/views.publish?user_id=#{user.uid}&view=#{encoded_msg}&pretty=1").parsed
+    end
+
+    def encoded_help_block_msg
+      msg = "{ 'type': 'home', 'blocks': [ { 'type': 'divider' }, { 'type': 'section', 'text': { 'type': 'mrkdwn', 'text': '*Slashコマンドについて*\n本アプリでは以下のコマンドがSlack内で利用できるよ:hamster:' } }, { 'type': 'section', 'fields': [ { 'type': 'mrkdwn', 'text': ':information_source: `/prof_help` \nDMでヘルプメッセージを送るよ' }, { 'type': 'mrkdwn', 'text': ':postbox: `/prof_random_block` \n DMでランダムにブロックを1つ送るよ' } ] }, { 'type': 'section', 'fields': [ { 'type': 'mrkdwn', 'text': ':ok_hand: `/prof_activate_share` \n毎日18時の投稿を有効するよ' }, { 'type': 'mrkdwn', 'text': ':raised_back_of_hand: `/prof_inactivate_share` \n 毎日18時の投稿を止めるよ' } ] }, { 'type': 'divider' } ] }"
       encoded_msg = ERB::Util.url_encode(msg)
       return encoded_msg
     end
