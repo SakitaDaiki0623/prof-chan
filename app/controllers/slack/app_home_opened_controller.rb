09bf7d934251ffc5f5e0e0d456b8f0a694d36d9d
sakita daiki <kurukuruskt28@gmail.com>
2021-06-19 12:28:40 +0900

    update:controller

--- app/controllers/slack/app_home_opened_controller.rb
+++ app/controllers/slack/app_home_opened_controller.rb
@@ -3,12 +3,12 @@ module Slack
     before_action :set_user_team_token, only: %i[publish]
 
     def respond
-      render json: params[:challenge], status: 200
-      # if params[:event][:type] == "app_home_opened"
-      #   views_publish
-      # elsif params[:event][:type] == 'message' && params[:event][:text].present?
-      #   send_help_msg
-      # end
+      # render json: params[:challenge], status: 200
+      if params[:event][:type] == "app_home_opened"
+        views_publish
+      elsif params[:event][:type] == 'message' && params[:event][:text].present?
+        send_help_msg
+      end
     end
 
     def views_publish
