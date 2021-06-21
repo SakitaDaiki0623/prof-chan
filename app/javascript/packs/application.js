faee55ebd0e9a56c13513442fcd76f358c07b334
sakita daiki <kurukuruskt28@gmail.com>
2021-06-21 10:44:47 +0900

    add:twitterアイコンを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,6 @@ require("@rails/activestorage").start();
 require("channels");
 
 import "stylesheets/application.scss";
+import '@fortawesome/fontawesome-free/js/all'
 import "../css/tailwind.scss";
 import "../css/main.scss";
