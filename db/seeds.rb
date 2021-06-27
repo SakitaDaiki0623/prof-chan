128ad4a946346048256034d002092d7a858ac326
sakita daiki <kurukuruskt28@gmail.com>
2021-06-27 18:06:14 +0900

    update:開発環境でのテストデータを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,41 @@
-# require 'factory_bot'
-# require 'faker'
+require 'factory_bot'
+require 'faker'
 
-# # create another team workspace and users belonging to it
-# if Rails.env.development?
+# create another team workspace and users belonging to it
+if Rails.env.development?
 
-#   FactoryBot.create(:team) do |team|
-#     FactoryBot.create_list(:user, 30, team: team)
-#   end
 
-#   # create real team workspace and users
-#   FactoryBot.create(:team, :real_team) do |team|
-#     FactoryBot.create_list(:user, 30, team: team)
-#     random_five_users = User.all.shuffle.slice(0, 5)
-#     random_ten_question_blocks = QuestionBlock.all.shuffle.slice(0, 10)
-#     random_ten_ranking_blocks = RankingBlock.all.shuffle.slice(0, 10)
-#     random_ten_text_blocks = TextBlock.all.shuffle.slice(0, 10)
-#     random_ten_yes_or_no_blocks = YesOrNoBlock.all.shuffle.slice(0, 10)
-#     random_five_users.each do |user|
-#       random_ten_question_blocks.each do |block|
-#         FactoryBot.create(:question_block_like, user: user, question_block: block)
-#       end
-#       random_ten_ranking_blocks.each do |block|
-#         FactoryBot.create(:ranking_block_like, user: user, ranking_block: block)
-#       end
-#       random_ten_text_blocks.each do |block|
-#         FactoryBot.create(:text_block_like, user: user, text_block: block)
-#       end
-#       random_ten_yes_or_no_blocks.each do |block|
-#         FactoryBot.create(:yes_or_no_block_like, user: user, yes_or_no_block: block)
-#       end
-#     end
-#   end
-#   # 管理者の作成
-#   FactoryBot.create(:user, :admin)
-# end
+
+  # create real team workspace and users
+  FactoryBot.create(:team, :normal_team) do |team|
+    FactoryBot.create(:user, :admin, team: team)
+    FactoryBot.create_list(:user, 30, team: team)
+    random_five_users = User.all.shuffle.slice(0, 5)
+    random_ten_question_blocks = QuestionBlock.all.shuffle.slice(0, 10)
+    random_ten_ranking_blocks = RankingBlock.all.shuffle.slice(0, 10)
+    random_ten_text_blocks = TextBlock.all.shuffle.slice(0, 10)
+    random_ten_yes_or_no_blocks = YesOrNoBlock.all.shuffle.slice(0, 10)
+    random_five_users.each do |user|
+      random_ten_question_blocks.each do |block|
+        FactoryBot.create(:question_block_like, user: user, question_block: block)
+      end
+      random_ten_ranking_blocks.each do |block|
+        FactoryBot.create(:ranking_block_like, user: user, ranking_block: block)
+      end
+      random_ten_text_blocks.each do |block|
+        FactoryBot.create(:text_block_like, user: user, text_block: block)
+      end
+      random_ten_yes_or_no_blocks.each do |block|
+        FactoryBot.create(:yes_or_no_block_like, user: user, yes_or_no_block: block)
+      end
+    end
+  end
+  # 管理者の作成
+
+  FactoryBot.create(:team, :real_team) do |team|
+    FactoryBot.create_list(:user, 30, :slack, team: team)
+  end
+end
 
 if Rails.env.production?
   Team.create!(
