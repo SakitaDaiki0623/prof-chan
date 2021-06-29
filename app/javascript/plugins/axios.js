3877b3862741e4f373b4be41174a4ed3cad1900e
sakita daiki <kurukuruskt28@gmail.com>
2021-06-29 14:16:22 +0900

    add:csrf-token

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,5 +1,12 @@
 import axios from "axios";
 
+axios.defaults.headers.common = {
+  "X-Requested-With": "XMLHttpRequest",
+  "X-CSRF-TOKEN": document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content"),
+};
+
 const axiosInstance = axios.create({
   baseURL: "/api/v1",
 });
