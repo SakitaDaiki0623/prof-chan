5ce441e5a6736004fd619fc01d23e25c4d599abd
sakita daiki <kurukuruskt28@gmail.com>
2021-07-01 18:39:10 +0900

    update:delete challnge

--- app/controllers/slack/app_home_opened_controller.rb
+++ app/controllers/slack/app_home_opened_controller.rb
@@ -1,12 +1,12 @@
 module Slack
   class AppHomeOpenedController < Slack::ApplicationController
     def respond
-      render json: params[:challenge], status: 200
-      # if params[:event][:type] == 'app_home_opened'
-      #   views_publish
-      # elsif params[:event][:type] == 'message' && params[:event][:text].present?
-      #   send_help_msg
-      # end
+      # render json: params[:challenge], status: 200
+      if params[:event][:type] == 'app_home_opened'
+        views_publish
+      elsif params[:event][:type] == 'message' && params[:event][:text].present?
+        send_help_msg
+      end
     end
 
     def views_publish
